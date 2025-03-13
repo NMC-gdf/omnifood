aa1da8c75dcb93c5ad691ed7fbd0139e05525d3d
NMC-GDF <ndunwachinedu001@gmail.com>
2025-03-13 15:36:11 +0100

    Mad correction in JS

--- js/script.js
+++ js/script.js
@@ -65,30 +65,24 @@ allLinks.forEach(function (link) {
   });
 });
 
-// sticky Navigation
+// Sticky Navigation
 const sectionHeroEl = document.querySelector(".section-hero");
-
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
     // console.log(ent);
 
-    if (ent.isIntersecting === false) {
-      document.querySelector(".header").classList.add("sticky");
+    if (ent.isIntersecting === false)
+      // document.querySelector(".header").classList.add("sticky");
       document.body.classList.add("sticky");
-    }
 
-    if (ent.isInteresting === true) {
-      document.body.classList.remove("sticky");
-    }
+    if (ent.isIntersecting === true) document.body.classList.remove("sticky");
   },
-
   {
-    // In the Viewport
+    // In the ViewPort
     root: null,
     threshold: 0,
     rootMargin: "-80px",
   }
 );
-
 obs.observe(sectionHeroEl);
